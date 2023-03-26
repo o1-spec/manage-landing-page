b23cc608593b0a12cda577a90afe204d3b4e650d
o1-spec <oluwafemionadokun@gmail.com>
2023-03-26 01:01:40 +0100

    Implemented testimonial carousel

--- manage-landing-page-master/script.js
+++ manage-landing-page-master/script.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const btn = document.getElementsByClassName('btn')
 const hamburger = document.querySelector('.hamburger')
 const mobile = document.querySelector('.mobile-links')
 const btnMobile = document.querySelector('.btn-mobile')
@@ -8,6 +9,7 @@ const body = document.getElementsByTagName('body')
 const mobileLink = document.querySelectorAll('.mobile-link')
 const overlay = document.querySelector('.overlay')
 const date = document.querySelector('.date')
+const slide = document.getElementById('slide')
 
 const now = new Date
 const day = now.getFullYear()
@@ -25,3 +27,36 @@ mobileLink.forEach(function(btn){
         overlay.classList.toggle('hidden')
     })
 })
+
+
+btn[0].onclick = function(e){
+    slide.style.transform = 'translateX(0px)'
+    for(let i =0; i< 4; i++){
+        btn[i].classList.remove('color')
+    }
+    this.classList.add('color')
+}
+
+btn[1].onclick = function(e){
+    slide.style.transform = 'translateX(-400px)'
+    for(let i =0; i< 4; i++){
+        btn[i].classList.remove('color')
+    }
+    this.classList.add('color')
+}
+
+btn[2].onclick = function(e){
+    slide.style.transform = 'translateX(-800px)'
+    for(let i =0; i< 4; i++){
+        btn[i].classList.remove('color')
+    }
+    this.classList.add('color')
+}
+
+btn[3].onclick = function(e){
+    slide.style.transform = 'translateX(-1200px)'
+    for(let i =0; i< 4; i++){
+        btn[i].classList.remove('color')
+    }
+    this.classList.add('color')
+}
